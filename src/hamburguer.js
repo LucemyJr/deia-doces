03295885614704258c14f24f42c5fd79f73991bf
Lucemy Ferreira <97321519+LucemyJr@users.noreply.github.com>
2022-09-18 11:46:00 -0300

    Updating the Mobile Menu

--- src/hamburguer.js
+++ src/hamburguer.js
@@ -1,8 +1,28 @@
 const btnMobile = document.getElementById('btn-mobile')
+const menuOption1 = document.getElementById('menuOption1')
+const menuOption2 = document.getElementById('menuOption2')
+const menuOption3 = document.getElementById('menuOption3')
+const menuOption4 = document.getElementById('menuOption4')
+const nav = document.getElementById('nav')
 
 function toggleMenu(){
-    const nav = document.getElementById('nav')
+    nav.classList.toggle('active') 
+}
+function toggleMenu1(){
+    nav.classList.toggle('active') 
+}
+function toggleMenu2(){
+    nav.classList.toggle('active') 
+}
+function toggleMenu3(){
+    nav.classList.toggle('active') 
+}
+function toggleMenu4(){
     nav.classList.toggle('active') 
 }
 
 btnMobile.addEventListener('click', toggleMenu)
+menuOption1.addEventListener('click', toggleMenu1)
+menuOption2.addEventListener('click', toggleMenu2)
+menuOption3.addEventListener('click', toggleMenu3)
+menuOption4.addEventListener('click', toggleMenu4)
